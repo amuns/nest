8fe6a56976e646c1b9e469a29fe037ead84fe835
amuns <amunpote@gmail.com>
2022-05-31 21:19:28 +0545

    upload

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,  //extra field passed will be filtered
+  }));
+  await app.listen(3001);
+}
+bootstrap();
